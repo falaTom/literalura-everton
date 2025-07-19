6dab6d7f1219d4380af2130c6757e6fba9f3fdd7
rafaelclima <rafaelclima.ti@gmail.com>
2025-07-19 20:04:04 -0300

    feat: criando repositorios

--- src/main/java/br/com/rafaellima/literalura/repository/AutorRepository.java
+++ src/main/java/br/com/rafaellima/literalura/repository/AutorRepository.java
@@ -1,7 +1,24 @@
 package br.com.rafaellima.literalura.repository;
 
+import br.com.rafaellima.literalura.dto.AutorLivroDTO;
 import br.com.rafaellima.literalura.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AutorRepository extends JpaRepository<Autor, Long> {
+   Autor findByNome(String name);
+
+   @Query("""
+  SELECT new br.com.rafaellima.literalura.dto.AutorLivroDTO(
+    a.nome, a.anoNascimento, a.anoFalecimento, l.titulo
+  )
+  FROM Autor a
+  JOIN a.livros l
+  WHERE :ano BETWEEN a.anoNascimento AND a.anoFalecimento
+""")
+   List<AutorLivroDTO> buscarAutoresVivosPorAno(@Param("ano") Integer ano);
+
 }
--- src/main/java/br/com/rafaellima/literalura/repository/LivroRepository.java
+++ src/main/java/br/com/rafaellima/literalura/repository/LivroRepository.java
@@ -3,5 +3,8 @@ package br.com.rafaellima.literalura.repository;
 import br.com.rafaellima.literalura.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LivroRepository extends JpaRepository<Livro, Long> {
+   List<Livro> findByIdiomaIgnoreCase(String idioma);
 }
